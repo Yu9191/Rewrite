1261108dcdad3c890d71eb0aa6e075c22b602852
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-09-12 19:46:49 +0800

    Create peak.js

---
+++ peak.js
@@ -0,0 +1,12 @@
+/*
+ * Peak
+ * 解锁Pro终身
+
+[rewrite_local]
+https://billing.peakcloud.org/billing/2/user/me url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/peak.js
+
+[mitm]
+hostname = billing.peakcloud.org
+
+*/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('H 3={"q":{"8":"9","m":1,"7":"l","k":1,"j":[{"7":"i","h":"6.5.g.4.e","f":{"2":0,"n":0,"d":{"c":"6.5.4.b.o","x":1,"p":"F","E":"D"},"8":"9","C":B,"A":z,"G":y}}]},"w":{"v":0}}$u({t:s.r(3),2:a});',44,44,'|false|status|obj|peak|brainbow|com|class|bbuid|zubwzoyjmS|200|substd0823|id|source|PeakModule|subscription|module|name|BillingModuleResponse|modules|is_in_billing_retry|UserModuleBillingResponse|usedIntroOfferPeriod|statusdate|lifetime|provider|response|stringify|JSON|body|done|code|metaResponse|cancelable|true|1726140838862|startTime|1726745623000|endTime|trial|type|itunes|pro|let'.split('|'),0,{}))
