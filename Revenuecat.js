e5fee8d7414d06df881cea803b730da69b6493c2
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-09-18 03:54:05 +0800

    Update Revenuecat.js
    Phorase

--- Revenuecat.js
+++ Revenuecat.js
@@ -64,6 +64,7 @@ if (typeof $response == "undefined") {
   const app = '1';
   const UAMappings = {
     'Opal':{ name: 'premium_tier_2', id: 'com.withopal.opal.premiumtier2lifetime'},//9.11
+    'Photoooo':{ name: 'lifetime', id: 'canoe_28_rnb_forever'},//9.18
     'Baby%20Generator':{ name: 'premium_features', id: 'babygenerator_499_weekly'},//9.9
     'Boring':{ name: 'pro', id: 'month'},//8.29lifelog
     'Snipd':{ name: 'premium', id: 'test_snipd_premium_grandfather_1y_4200_trial_2w_v1'},//9.10
