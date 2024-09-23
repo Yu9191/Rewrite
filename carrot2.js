03fa008103f140fab4917d1754eb6fca88ca36e4
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-09-23 19:46:25 +0800

    Create carrot2.js

---
+++ carrot2.js
@@ -0,0 +1,11 @@
+
+let headers = $request.headers;
+
+
+headers['x-parse-request-id'] = 'EADBCD55-AAD3-4898-B55E-DDAF1507AC64';
+headers['x-parse-application-id'] = 'u8vDnXgRnRuxAdoaAPwNQ9C5fm2GtfX25PsJFAuX';
+headers['x-parse-client-key'] = '';  
+headers['x-parse-installation-id'] = 'e8bb1843-ab5d-4470-a1c1-1b9863522489';
+
+
+$done({ headers });
