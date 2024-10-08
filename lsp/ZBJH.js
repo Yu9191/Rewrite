238eca537df7eec5394d0d2b8d19dfe07c1079c3
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-10-08 14:27:28 +0800

    Create ZBJH.js

---
+++ lsp/ZBJH.js
@@ -0,0 +1,11 @@
+/*
+ * 直播聚合
+ * https://lanzouw.com/s/33th 邀请码:T2775
+ 
+[rewrite_local]
+^http:\/\/lb\.h12345\.net\/zb\/.*\.json url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/lsp/zhibojuhe.js
+
+
+[mitm]
+hostname = lb.h12345.net
+*/
