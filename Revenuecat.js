4b4f4ad1d96f2ca72f005996e91cfbd0917aa0d4
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-11-11 00:11:20 +0800

    更新 Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -63,6 +63,7 @@ if (typeof $response == "undefined") {
   var UA = $request.headers['user-agent'];
   const app = '1';
   const UAMappings = {
+    'ChatPub':{name:'Unlimited Access',id:'conversationai.year'},//11.11
     'Unfold':{name:'FF2_STORY',id:'UNFOLD_PRO_YEARLY'},//11.2
     'Origami':{name:'Premium',id:'origami_499_1m'},//11.1
     'Reader':{name:'standard',id:'vd_monthly_999'},//10.27
