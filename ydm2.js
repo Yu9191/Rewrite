f7d56dfafe8ece3cb3e5cd50c7b87cc50020de4c
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-03-26 22:47:41 +0800

    Update ydm2.js
    2025-03-26 增加新地址

--- ydm2.js
+++ ydm2.js
@@ -1,14 +1,18 @@
-/* 
+/*
  * 2025-03-11 适配小火箭
+ * 2025-03-26 增加新地址
 */
 const url = $request.url;
 const isQX = typeof $task !== "undefined";
 const apiUrls = {
     'd2x03a61ogs2x5.cloudfront.net': 'https://vpn3.lovebabyforever.workers.dev/?videoId=300000',
     'd3lijns9322mkl.cloudfront.net': 'https://vpn2.lovebabyforever.workers.dev',
-    'dd38dkt7dfvyr.cloudfront.net': null 
+    'dd38dkt7dfvyr.cloudfront.net': null,
+    'd1xwta4tq724e9.cloudfront.net': null // 萌萝社
 };
 const Gu = '1741105775-19918480-32-d52445865e1cb896e73d6d001044f961';
+const Mls = '1742999714-130991972-8-912c035553aba46669633c95572ef571'; //萌萝社
+
 if (url.includes('auth_key=')) {
     let newAuthKey = null;
     let apiUrl = null;
@@ -19,6 +23,8 @@ if (url.includes('auth_key=')) {
         apiUrl = apiUrls['d3lijns9322mkl.cloudfront.net'];
     } else if (url.includes('dd38dkt7dfvyr.cloudfront.net')) {
         newAuthKey = Gu;
+    } else if (url.includes('d1xwta4tq724e9.cloudfront.net')) {
+        newAuthKey = Mls; // 萌萝社
     }
 
     if (apiUrl) {
