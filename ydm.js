068bb9c21371b5925a316e73d8e0daafcbf11a20
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-03-27 15:58:47 +0800

    Update ydm.js
    修改正则匹配 避免误杀

--- ydm.js
+++ ydm.js
@@ -11,7 +11,7 @@ https:\/\/[^\/]+\.cloudfront\.net\/api\/m3u8\/decode\/authPath url script-reques
 # 会员
 ^https:\/\/(?:[a-zA-Z0-9-]+\.cloudfront\.net|snerbnew\.pxyzjmspfl\.work)(?::\d+)?\/api\/(?:video\/getVideoById|user\/base\/info|community\/dynamic\/dynamicInfo) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/ydm.js
 # 广告 
-^https://(?:[a-zA-Z0-9-]+\.cloudfront\.net|snerbnew\.pxyzjmspfl\.work)(?::\d+)?/api/?(?:sys/partner/list|activity/indexActs|sys/advertisement/list|sys/getImgAndVideoCdnList|aibox/entranceConfig)? url reject
+^https://(?:[a-zA-Z0-9-]+\.cloudfront\.net|snerbnew\.pxyzjmspfl\.work)(?::\d+)?/api/?(?:sys/partner/list|activity/indexActs|sys/advertisement/list|sys/getImgAndVideoCdnList|aibox/entranceConfig)?$ url reject
 #三年二班 
 ^https:\/\/snerbnew\.pxyzjmspfl\.work(?::\d+)?\/api\/m3u8\/decode\/authPath url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/ydm2.js
 [mitm]
