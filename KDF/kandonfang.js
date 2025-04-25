cb93bf71b2f1612404b636cc6cc5509b7406200a
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-04-25 13:11:41 +0800

    Update kandonfang.js
    更换域名.增加去除开屏广告以及首页弹窗广告

--- KDF/kandonfang.js
+++ KDF/kandonfang.js
@@ -1,8 +1,11 @@
 [rewrite_local]
 # NBA视频和电视剧
-https:\/\/bp-api\.bestv\.com\.cn\/cms\/api\/(live\/studio\/id\/v4|c\/player\/common) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/KDF/video-parser.js
+https:\/\/bp-api\.bestv\.(?:com\.cn|cn)\/cms\/api\/(live\/studio\/id\/v4|c\/player\/common) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/KDF/video-parser.js
 # HTML页面
 ^https:\/\/360\.com\/video url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/KDF/video-parser.js
-
+# 首页弹窗广告
+^https:\/\/bp-api\.bestv\.cn\/cms\/api\/advertise\/project\/advertise\/popup\/scene$ url reject
+# 开屏广告（清除缓存）
+https://bp-api.bestv.cn/cms/api/free/open/advertisingV2 url reject
 [mitm]
-hostname = 360.com, bp-api.bestv.com.cn
+hostname = 360.com, bp-api.bestv.com.cn, bp-api.bestv.cn
