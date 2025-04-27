81789e63c5a7a032004a849dc0418f9b433ba0e9
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-04-27 16:58:27 +0800

    Update AppRavenTask.js

--- AppRavenTask.js
+++ AppRavenTask.js
@@ -1,19 +1,4 @@
-/* 
-// AppRaven 14‑Day Premium (QX / Surge / Loon)
-// --------------------------------------------------------
-// Author: baby 
-// 支持批量生成多组会员账号
-// === 配置说明 ===
-// 1. 修改下面 COUNT 数值 (1‑5)
-// 2. 加入定时任务即可, 每14天8点执行 需要可以自己修改
-// 示例如下：   - Quantumult X : 0 8 */14 * * AppRaven.js, tag=AppRaven.js
-//             - Surge / Loon:  cron "0 8 */14 * *" script-path=AppRaven.js
-// --------------------------------------------------------
-[task_local]
-0 8 */14 * * https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/AppRavenTask.js, tag=AppRaven会员账号生成
 
-
-*/
 const COUNT = 2;   // ⚙️ 生成几组会员账号 最多4
 
 const $ = new Env('AppRaven会员生成');
