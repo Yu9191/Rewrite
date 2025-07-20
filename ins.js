cad606e3f8255c8d03d644257542d75ecc4f6770
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-07-21 03:16:12 +0800

    Create ins.js

---
+++ ins.js
@@ -0,0 +1,19 @@
+/* 
+ins社区
+
+[rewrite_local]
+
+https:\/\/x2\.ins620\.com\/post\/app\/p\/post\/private\/(info|page|similar\/page) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insck.js
+https://x2.ins620.com/party/common/advertising/public/adv/list url reject
+https://x2.ins620.com/user/app/u/user/private/info url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/ins.js
+[mitm]
+hostname = x2.ins620.com
+*/
+
+let obj = JSON.parse($response.body);
+obj.data.userName = "baby";
+obj.data.nickName = "baby";
+obj.data.vipLevel = 9;
+obj.data.vipStartTime = 1753035900477; 
+obj.data.vipEndTime = 4102444800000; 
+$done({ body: JSON.stringify(obj) });
