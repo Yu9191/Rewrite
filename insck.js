01eca05577acf23082cff217255a527d5eac8a96
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-07-21 03:17:47 +0800

    更新 insck.js token

--- insck.js
+++ insck.js
@@ -1,4 +1,4 @@
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2Vy';
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMjUwNzIxMDAwMjExNiIsImxvZ2luX3R5cGUiOjMsImlzcyI6ImgiLCJ1c2VyX2tleSI6IjI1MDcyMTAwMDIxMTYiLCJpYXQiOjE3NTMwMzkwNjUsInVzZXJuYW1lIjoiYmFieTkyOTI5In0.TUcADe5pJBt-mCrYEIxvHzHyBdcLjxvy7zAtT-YSviLy1G3F2eWGALhd-2mevJoHNS7P0g3fDwfp0q_Z60pj7w';
 let headers = $request.headers;
 
 if ('authorization' in headers || 'Authorization' in headers) {
