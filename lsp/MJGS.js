94382f94e38df67c5835566158235a53f6903077
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-07-21 08:53:58 +0800

    更新 MJGS.js
    修复短视频正则

--- lsp/MJGS.js
+++ lsp/MJGS.js
@@ -5,7 +5,7 @@
 #游戏
 ^https?:\/\/(mfjk|gnjk\d*|gnjk3|jk|buyaoda).+\.com\/request_game\/v3\/game\/list.*$ url reject-dict
 #视频
-^https?:\/\/(mfjk|gnjk\d*|buyaoda|gnjk2\.kzfbz|jk\.bnhwq672q3)\..*\.com\/(api\/(comic|video|short_movie_info|novel|gallery|vip|index|video\/info)|request_rust_video\/ai\/init_info).*$ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgshd.js
+^https?:\/\/(mfjk|gnjk\d*|buyaoda|gnjk2\.kzfbz|jk\.bnhwq672q3)\.[a-zA-Z0-9-]+\.com\/(api\/(comic|video|short_movie_info|short_movie_lock|novel|gallery|vip|index|video\/info)|request_rust_video\/ai\/init_info).*$ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgshd.js
 
 [mitm]
 hostname = gnjk.*.com, mfjk.*.com, buyaoda.*.com, jk.*.com, gnjk3.*.com, gnjk1.*.com, gnjk2.*.com
