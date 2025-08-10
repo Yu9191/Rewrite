9676c40b5aaf17d3ffca7503b81fbbf53157c533
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-08-11 00:10:15 +0800

    后端自动更新token

--- insck.js
+++ insck.js
@@ -1,4 +1,4 @@
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMjUwODEwMDAwMDI0NSIsImxvZ2luX3R5cGUiOjMsImlzcyI6ImgiLCJ1c2VyX2tleSI6IjI1MDgxMDAwMDAyNDUiLCJpYXQiOjE3NTQ3NTU4MDQsInVzZXJuYW1lIjoiYmFieXNpZnJnIn0.97Z8EcxKR17CxuPPckbKrv48nAS8UOvIc6HS5cE9ka-pnYJUouqzrPhJrAr71pycj1LlANwILzOa6I-7UysJKA';
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMjUwODExMDAwMDIxOSIsImxvZ2luX3R5cGUiOjMsImlzcyI6ImgiLCJ1c2VyX2tleSI6IjI1MDgxMTAwMDAyMTkiLCJpYXQiOjE3NTQ4NDIyMDIsInVzZXJuYW1lIjoiYmFieTNpaDFvIn0.7o7k23fuPPOYF26P71mWgkEGn6VVnhqzT241a5GKdcYYcyFTOov8ROciB9YrPVH2UtK5Bs-Q-C-pEGRG807ZeQ';
 let headers = $request.headers;
 
 if ('authorization' in headers || 'Authorization' in headers) {
