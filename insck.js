3cfe91e2dc43465c96efcaa9aeb4eb9edf0178fc
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-11-19 20:17:18 +0800

    Update insck.js token

--- insck.js
+++ insck.js
@@ -1,10 +1,9 @@
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMjUxMTE5MDAwNTQ5OCIsImxvZ2luX3R5cGUiOjMsImlzcyI6ImgiLCJ1c2VyX2tleSI6IjI1MTExOTAwMDU0OTgiLCJpYXQiOjE3NjM1NTQzNjEsInVzZXJuYW1lIjoidmlzaXRvcl8yNTExMTkwMDA1NDk4In0.yMMcKEehWNGsFVO76PVw29kqo2uFEpeBAkAqqumYZZEpPtNlTNImr3Bn_RUF-0waA9B-yz2-1LiOQXjZ_15Osw';
+
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMjUxMTE5MDAwNTUxOSIsImxvZ2luX3R5cGUiOjMsImlzcyI6ImgiLCJ1c2VyX2tleSI6IjI1MTExOTAwMDU1MTkiLCJpYXQiOjE3NjM1NTQ2MjQsInVzZXJuYW1lIjoidmlzaXRvcl8yNTExMTkwMDA1NTE5In0.PDZ3XJl-jkONtjSWlIc7qJ2sYNllvhlfw0dlKZcJgi0JhVZ7cB9qgsJ4wSCRD0MtokhhoZvccs6ztbwQwNmXBg';
 
 let headers = $request.headers;
-
 if ('authorization' in headers || 'Authorization' in headers) {
   const key = headers.authorization ? 'authorization' : 'Authorization';
   headers[key] = token;
 }
-
 $done({ headers });
