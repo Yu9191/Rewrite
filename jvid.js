f54288860905f98470ceafc3f6ac4f22c2f15835
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2025-12-01 00:10:38 +0800

    [AutoUpdate] JVID domains - 2025-12-01

--- jvid.js
+++ jvid.js
@@ -11,7 +11,7 @@
 #^https?://[^/]+/front/(media/(?:listCommentByMediaId|getMediaById|listMediaBySearchType|listAllBySecondCategoryIdNew)|system/vipsetting/getVipSetWithPayType)$ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/jvid2.js
 
 [mitm]
-hostname = *.cdv6x4x6.cc, *.cdv6x4x6.cn, *.dgpes3dp.cc, *.hwyvspnp.top, *.jgx9fbnk.top, *.uywhqqxp.cc
+hostname = *.cdv6x4x6.cc, *.cdv6x4x6.cn, *.csaqsdhq.top, *.dgpes3dp.cc, *.gn5va64k.top, *.uywhqqxp.cc
 */
 const $ = new Env('JVID大陆版', {
     "logLevel": "info"
