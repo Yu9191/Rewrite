cec8bab2d35ea5a3e6b8ace9fcbf07385dd2dc28
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2026-03-01 20:56:34 +0800

    适配最新无码地址

--- insav.tv.js
+++ insav.tv.js
@@ -1,12 +1,12 @@
 /*
  * 无码平台不支持
 [rewrite_local]
-^https?:\/\/(insav\.tv|4k\.insav\.tv|dm\.insav\.tv|.*\.rftn469\.com)\/api\/(extend\/lists|config\/lists|config\/links|notice\/lists|user\/(getVipStatus|info)) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insav.tv.js
+^https?:\/\/(insav\.tv|4k\.insav\.tv|dm\.insav\.tv|.*\.rftn469\.com|.*\.pbfk364\.com)\/api\/(extend\/lists|config\/lists|config\/links|notice\/lists|user\/(getVipStatus|info|login)) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insav.tv.js
 
-^https?:\/\/(insav\.tv|4k\.insav\.tv|dm\.insav\.tv|.*\.rftn469\.com)\/api\/video\/getVideoUrl url script-request-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insav.tv.js
+^https?:\/\/(insav\.tv|4k\.insav\.tv|dm\.insav\.tv|.*\.rftn469\.com|.*\.pbfk364\.com)\/api\/video\/getVideoUrl url script-request-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insav.tv.js
 
 [mitm]
-hostname = insav.tv, 4k.insav.tv, dm.insav.tv, *.rftn469.com
+hostname = insav.tv, 4k.insav.tv, dm.insav.tv, *.rftn469.com, *.pbfk364.com
 
 *
 */
