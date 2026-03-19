d5224613cc491f72dfa3ce93a10e313efaa7666e
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2026-03-18 23:39:41 -0400

    Update hostname config - add wuma.mejj577.com, wuma.qhcf928.com, wuma.afvc953.com, wuma.tqnh257.com - 2026/3/19 11:39:28

--- insav.tv.js
+++ insav.tv.js
@@ -6,7 +6,7 @@
 ^https?:\/\/(insav\.tv|4k\.insav\.tv|dm\.insav\.tv|wuma\..*\.com|.*\.[a-z]{4}\d{3}\.com)\/api\/video\/getVideoUrl url script-request-body https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/insav.tv.js
 
 [mitm]
-hostname = insav.tv, 4k.insav.tv, dm.insav.tv, *.afvc953.com, *.jkfa588.com, *.mejj577.com, *.pbfk364.com, *.qhcf928.com, *.rahc448.com, *.rftn469.com, wuma.*.com, wuma.jkfa588.com
+hostname = insav.tv, 4k.insav.tv, dm.insav.tv, *.afvc953.com, *.jkfa588.com, *.mejj577.com, *.pbfk364.com, *.qhcf928.com, *.rahc448.com, *.rftn469.com, *.tqnh257.com, wuma.*.com, wuma.jkfa588.com
 
 
 *
