e778266228658bf293a6711826c458744ee02393
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2026-06-11 05:51:33 +0000

    insav: 构建时间改为北京时间

--- insav-tv/rollup.config.js
+++ insav-tv/rollup.config.js
@@ -3,7 +3,7 @@ import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 
-const now = new Date();
+const now = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Shanghai' }));
 const buildTime = `${now.getFullYear()}-${String(now.getMonth()+1).padStart(2,'0')}-${String(now.getDate()).padStart(2,'0')} ${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')}`;
 
 export default [
